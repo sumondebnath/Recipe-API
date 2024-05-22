c0a931c863c8a90e9c52af1080b45b4db45d4f8d
sumondebnath <debnathsumon1708@gmail.com>
2024-05-23 03:36:12 +0600

    new updated

--- recipe_share_application/views.py
+++ recipe_share_application/views.py
@@ -7,3 +7,11 @@ class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+
+    #     user_id = self.request.query_params.get("user_id")
+    #     if user_id:
+    #         queryset = queryset.filter(user_id = user_id)
+    #     return queryset
