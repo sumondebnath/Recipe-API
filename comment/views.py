695333849fff0dd27451bdfdb62f6a96da6b060c
sumondebnath <debnathsumon1708@gmail.com>
2024-05-23 14:23:47 +0600

    comment update

--- comment/views.py
+++ comment/views.py
@@ -9,3 +9,15 @@ class CommentViewset(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user_id = self.request.query_params.get("user_id")
+        recipe_id = self.request.query_params.get("recipe_id")
+        # print(user_id, recipe_id)
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        if recipe_id:
+            queryset = queryset.filter(recipe_id=recipe_id)
+        return queryset
